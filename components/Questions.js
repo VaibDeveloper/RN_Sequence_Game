0e1fc3062e4bbc0090e87ebbbe2a60b56ac79eca
VaibDeveloper <135333002+VaibDeveloper@users.noreply.github.com>
2023-08-01 19:13:37 +0530

    Updated Questions.js ( 7 Levels )

--- components/Questions.js
+++ components/Questions.js
@@ -1,21 +1,131 @@
 //import liraries
 
 const questions = [
-  {
-    Alphabets: ["A", "F", "J", "D"],
-  },
-  {
-    Alphabets: ["H", "I", "L", "X"],
-  },
-  {
-    Alphabets: ["Q", "W", "E", "R"],
-  },
-  {
-    Alphabets: ["S", "G", "V", "P"],
-  },
-  {
-    Alphabets: ["H", "O", "E", "Y"],
-  },
+  [
+    //Level1
+    {
+      Alphabets: ["A", "F"],
+    },
+    {
+      Alphabets: ["H", "I"],
+    },
+    {
+      Alphabets: ["Q", "W"],
+    },
+    {
+      Alphabets: ["S", "G"],
+    },
+    {
+      Alphabets: ["H", "O"],
+    },
+  ],
+  [
+    //Level2
+    {
+      Alphabets: ["A", "F"],
+    },
+    {
+      Alphabets: ["H", "I"],
+    },
+    {
+      Alphabets: ["Q", "W"],
+    },
+    {
+      Alphabets: ["S", "G"],
+    },
+    {
+      Alphabets: ["H", "O"],
+    },
+  ],
+  [
+    //Level3
+    {
+      Alphabets: ["A", "F", "J"],
+    },
+    {
+      Alphabets: ["H", "I", "L"],
+    },
+    {
+      Alphabets: ["Q", "W", "E"],
+    },
+    {
+      Alphabets: ["S", "G", "V"],
+    },
+    {
+      Alphabets: ["H", "O", "E"],
+    },
+  ],
+  [
+    //Level4
+    {
+      Alphabets: ["A", "F", "J"],
+    },
+    {
+      Alphabets: ["H", "I", "L"],
+    },
+    {
+      Alphabets: ["Q", "W", "E"],
+    },
+    {
+      Alphabets: ["S", "G", "V"],
+    },
+    {
+      Alphabets: ["H", "O", "E"],
+    },
+  ],
+  [
+    //Level5
+    {
+      Alphabets: ["A", "F", "J", "D"],
+    },
+    {
+      Alphabets: ["H", "I", "L", "X"],
+    },
+    {
+      Alphabets: ["Q", "W", "E", "R"],
+    },
+    {
+      Alphabets: ["S", "G", "V", "P"],
+    },
+    {
+      Alphabets: ["H", "O", "E", "Y"],
+    },
+  ],
+  [
+    //Level6
+    {
+      Alphabets: ["A", "F", "J", "D"],
+    },
+    {
+      Alphabets: ["H", "I", "L", "X"],
+    },
+    {
+      Alphabets: ["Q", "W", "E", "R"],
+    },
+    {
+      Alphabets: ["S", "G", "V", "P"],
+    },
+    {
+      Alphabets: ["H", "O", "E", "Y"],
+    },
+  ],
+  [
+    //Level7
+    {
+      Alphabets: ["A", "F", "J", "D", "Z"],
+    },
+    {
+      Alphabets: ["H", "I", "L", "X", "K"],
+    },
+    {
+      Alphabets: ["Q", "W", "E", "R", "M"],
+    },
+    {
+      Alphabets: ["S", "G", "V", "P", "S"],
+    },
+    {
+      Alphabets: ["H", "O", "E", "Y", "F"],
+    },
+  ],
 ];
-
 export default questions;
